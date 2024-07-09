9cba7c2f430d268a85cebaa2f11d316090099758
Brownie75 <gaelmolinareza@gmail.com>
2024-07-09 10:13:48 -0600

    API corregida

--- api_rest.js
+++ api_rest.js
@@ -1,12 +1,2 @@
-/* import {server, cors, bodyParser} from "./index.js";
+const server = require("./index.js");
 
-server.use(bodyParser.urlencoded({extended: false}));
-server.use(bodyParser.json());
-server.use(cors());
-
-server.get("/", (req,res) => {
-    console.log("GET /");
-    console.log(res.query);
-    res.send("Bienvenido a Chef en Casa");
-})
-*/
--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const bodyParser = require("body-parser");
 server.use(bodyParser.urlencoded({extended: false}));
 server.use(bodyParser.json());
 server.use(cors());
+server.use(express.static(__dirname + '/V.01-formulario'));
+server.use(express.static(__dirname + '/Support-form'));
 
 const conn = db.createConnection({
     host: "localhost",
@@ -24,58 +26,76 @@ server.listen(3000, () =>{
     console.log("Server is running on http://localhost:3000");
 });
 
-// API (Pendiente de cambiar)
+// API (Pendiente de cambiar a otro archivo)
 
 server.get("/", (req,res) => {
-    console.log("GET /");
-    console.log(res.query);
-    res.send("Bienvenido a Chef en Casa");
+  console.log("GET /");
+  console.log(res.query);
+  //res.send("Bienvenido a Chef en Casa");
+  res.sendFile("./V.01-formulario/index.html");
 })
 
 server.get("/user", (req, res) => {
-    conn.query("SELECT * FROM usuarios", (error, results) => {
-        if(error){
-            console.log("Error fetching data",error);
-            res.send("Error fetching data",500);
-          } else {
-            console.log("Data fetched succesfully");
-            res.send(results);
-          }
-    });
+  conn.query("SELECT * FROM usuarios", (error, results) => {
+      if(error){
+          console.log("Error fetching data",error);
+          res.send("Error fetching data",500);
+        } else {
+          console.log("Data fetched succesfully");
+          res.send(results);
+        }
+  });
 })
 server.get("/user/:id", (req, res) =>{
-    const id = req.params.id;
-    conn.query("SELECT * FROM usuarios WHERE id_user = ?",[id], (error, results) =>{
+  const id = req.params.id;
+  conn.query("SELECT * FROM usuarios WHERE id_user = ?",[id], (error, results) =>{
+  if(error){
+    console.log("Error fetching data", error);
+    res.send("Error fetching data", 500);
+  } else {
+    console.log("data fetch successfully");
+    res.send(results);
+  }
+});
+})
+server.get("/recipe/:id", (req, res) => {
+  const id = req.params.id;
+  conn.query("SELECT * FROM posts WHERE id_post = ?",[id],(error, results) => {
+      if(error){
+          console.log("Error fetching data", error);
+          res.send("Error fetching data", 500);
+        } else {
+          console.log("data fetch successfully");
+          res.send(results);
+        }
+  });
+})
+server.get("/user/:id/posts", (req,res) =>{
+  const id = req.params.id;
+  conn.query("SELECT * FROM posts WHERE fk_user = ?",[id],(error, results) => {
+      if(error){
+          console.log("Error fetching data", error);
+          res.send("Error fetching data", 500);
+        } else {
+          console.log("data fetch successfully");
+          res.send(results);
+        }
+  });
+})
+server.post("/user", (req, res) => {
+const {username, password_, correo} = req.body;
+conn.query("INSERT INTO usuarios (username, password_, correo) VALUES ('"
+  +username+"', '"+password_+"','"+correo+"')", (error, results) =>{
     if(error){
-      console.log("Error fetching data", error);
-      res.send("Error fetching data", 500);
+      res.send("Error inserting data", 500);
     } else {
-      console.log("data fetch successfully");
       res.send(results);
     }
   });
+});
+server.post("/user",(req,res)=> {
+
 })
-server.get("/recipe/:id", (req, res) => {
-    const id = req.params.id;
-    conn.query("SELECT * FROM posts WHERE id_post = ?",[id],(error, results) => {
-        if(error){
-            console.log("Error fetching data", error);
-            res.send("Error fetching data", 500);
-          } else {
-            console.log("data fetch successfully");
-            res.send(results);
-          }
-    });
-})
-server.get("/user/:id/posts", (req,res) =>{
-    const id = req.params.id;
-    conn.query("SELECT * FROM posts WHERE fk_user = ?",[id],(error, results) => {
-        if(error){
-            console.log("Error fetching data", error);
-            res.send("Error fetching data", 500);
-          } else {
-            console.log("data fetch successfully");
-            res.send(results);
-          }
-    });
-})
+/* server.get("/support",(req,res) => {
+res.sendFile("./Support-form/index.html")
+}) */
