87e44ee63b3d66c376b5294fb4f4af675074ccc6
Brownie75 <gaelmolinareza@gmail.com>
2024-08-06 07:33:21 -0600

    agregado diseño de posts

--- html/POSTS-1/new_script.js
+++ html/POSTS-1/new_script.js
@@ -28,7 +28,7 @@ image_input.addEventListener("change", async (event) => {
     var form = new FormData();
     form.append('id_post', getCookie("id_post"));
     form.append('image', image_input.files[0]);
-    await fetch("https://web-proyecto-integrador.onrender.com/image", {
+    await fetch("http://localhost:3000/image", {
         method: "POST",
         body: form
     }).then(response => response.text())
@@ -40,7 +40,6 @@ image_input.addEventListener("change", async (event) => {
 
 var id_post = new URLSearchParams(window.location.search).get("id_post")
 document.cookie = `id_post=`+id_post+`; path=/`;
-document.cookie = `username=`+getCookie("username")+`; path=/`;
 
 function putting_image(url) {
     var divs = document.querySelectorAll("#recipe div");
@@ -57,40 +56,6 @@ function putting_image(url) {
         div.appendChild(img);
         content.appendChild(div);
     }
-}
-
-function publish_post() {
-    var submit_content = document.getElementById("submit_form");
-    var prev_img = (content.querySelector("img")) ? content.querySelector("img").src : "";
-    var ingredients = (content.querySelector("ul")) ? content.querySelector("ul").innerHTML.trim() : "";
-    var hiddenInput = submit_content.querySelector("input[name='description']").value;
-
-    var formData = {
-        id_post: getCookie('id_post'),
-        username: getCookie('username'),
-        page_title: submit_content.recipe_title.value,
-        ingredients: ingredients,
-        page_content: content.innerHTML.trim(),
-        preview: prev_img,
-        description: hiddenInput
-    };
-
-    fetch("https://web-proyecto-integrador.onrender.com/post_recipe", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(formData)
-    }).then(response => response.json())
-      .then(data => {
-        console.log(data)
-        if (data) {
-            alert('Receta publicada!');
-            window.location.href = "/html/POSTS-1/publicado.html?id_post="+getCookie('id_post');
-        } else {
-            alert('Hubo un error al publicar la receta.');
-        }
-    });
 }
 
 // DESCRIPCIÓN DE LA RECETA
@@ -114,7 +79,7 @@ window.onclick = function(event) {
     }
 }
 
-saveBtn.onclick = function() {
+saveBtn.onclick = function(event) {
     var description = document.getElementById("post-description").value;
     if (description) {
         var hiddenInput = document.createElement("input");
@@ -123,9 +88,48 @@ saveBtn.onclick = function() {
         hiddenInput.value = description;
         document.getElementById("submit_form").appendChild(hiddenInput);
 
-        publish_post();
-        modal.style.display = "none"; 
+        publish_post(event);
+        // modal.style.display = "none"; 
     } else {
         alert("Por favor, ingrese una descripción.");
     }
 }
+
+function publish_post(event) {
+    event.preventDefault()
+    var description = document.getElementById("post-description").value;
+    if (description) {
+
+        let data = new FormData();
+        data.append("id_post", id_post);
+        data.append("page_title", document.querySelector("input[name='recipe_title']").value);
+        data.append("ingredients", (document.getElementById("recipe").querySelector("ul")) ? document.getElementById("recipe").querySelector("ul").innerHTML : "No ingredients");
+        data.append("page_content",document.getElementById("recipe").innerHTML);
+        data.append("preview", (document.getElementById("recipe").querySelector("img")) ? document.getElementById("recipe").querySelector("img").src : "/assets/pictures/example_pic.jpg");
+        data.append("description", description);
+
+        console.log(Object.fromEntries(data));
+
+        let _body = Object.fromEntries(data)
+
+        fetch("http://localhost:3000/post_recipe",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(_body)
+        }).then(response => response.json())
+        .then(data => {
+            if(data.estado == "Publicado!"){
+                window.location.href = "publicado.html?id_post="+id_post; 
+            }
+        })
+
+        /* localStorage.setItem('recipeTitle', document.querySelector("input[name='recipe_title']").value);
+        localStorage.setItem('recipeContent', document.getElementById("recipe").innerHTML);
+        localStorage.setItem('recipeDescription', description);*/
+    } else {
+        alert("Por favor, ingrese una descripción.");
+    }
+};
+
